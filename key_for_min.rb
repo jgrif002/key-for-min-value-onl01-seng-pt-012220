704f54a68f488e107f74a210012933b87b523401
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-02 03:55:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,3 +14,15 @@ def key_for_min_value(name_hash)
   min_key
 end
 
+def key_for_min_value(name_hash)
+  min_key = nil
+  min_value = 0
+  
+  name_hash.collect do |key, value|
+    if value < min_value || min_value == 0
+      min_value = value 
+      min_key = key
+      end
+    end
+  min_key
+end
